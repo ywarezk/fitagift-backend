8724a0389a0aa8dbe7f22bb6c3fefed9299fd480
OpenShift guest <ceo@ykapps.com>
2013-08-03 12:28:34 -0400

    installed sendgrid

--- fitagift_backend_app/settings.py
+++ fitagift_backend_app/settings.py
@@ -186,3 +186,13 @@ AWS_STORAGE_BUCKET_NAME = os.environ['FITAGIFT_ENV_AWS_STORAGE_BUCKET_NAME']
 
 #when sending mail the user will see this address
 FROM_EMAIL_ADDRESS = 'noreply@nerdeez.com'
+
+#for send grid
+try:
+    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    EMAIL_HOST= 'smtp.sendgrid.net'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+except:
+    pass
